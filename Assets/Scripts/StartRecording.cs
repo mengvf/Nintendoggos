efb552ac7be77941340c217c8c009921243abd09
Max <klinman.max@gmail.com>
2019-04-13 10:11:46 -0700

    dog animation

--- Assets/Scripts/StartRecording.cs
+++ Assets/Scripts/StartRecording.cs
@@ -7,10 +7,11 @@ public class StartRecording : MonoBehaviour
     AudioClip voice_command;
     public GameObject dog;
     public GameObject main_camera;
+    Animator dogAnim;
     // Start is called before the first frame update
     void Start()
     {
-
+        dogAnim = dog.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +22,6 @@ public class StartRecording : MonoBehaviour
             int min;
             int max;
             Microphone.GetDeviceCaps(Microphone.devices[0], out min, out max);
-            print(min);
-            print(max);
             voice_command = Microphone.Start(Microphone.devices[0], false, 2, min);
         }
         if (Input.GetKeyUp("t") == true)
@@ -30,7 +29,18 @@ public class StartRecording : MonoBehaviour
             Microphone.End(Microphone.devices[0]);
             //Voice has been detected. Move dog towards camera
             dog.transform.LookAt(new Vector3(main_camera.transform.position.x, dog.transform.position.y, main_camera.transform.position.z), new Vector3(0, 1, 0));
-            dog.GetComponent<Animator>().Play("attack");
+            dog.transform.Rotate(0, 90, 0);
+            print(dogAnim.HasState(0, Animator.StringToHash("Base Layer.run")));
+            dogAnim.Play(Animator.StringToHash("Base Layer.run"), 0);
         }
+        if (Input.GetKeyDown("s"))
+        {
+            StopDog();
+        }
+    }
+
+    void StopDog()
+    {
+        dog.transform.LookAt(new Vector3(main_camera.transform.position.x, dog.transform.position.y, main_camera.transform.position.z), new Vector3(0, 1, 0));
     }
 }
